7ef021bd4c0a62ddc326e268d136835400e36b7c
DevJtaiwo <taiwoayomide202@gmail.com>
2024-08-06 15:38:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require('cors');
+const dotenv = require('dotenv');
+const moongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit'); // Rate Limiting
+// const { createProxyMiddleware } = require('http-proxy-middleware'); // Reverse Proxy
+
+const userRoute = require("./route/user");
+const blogRoute = require("./route/blog");
+const ebookRoute = require("./route/ebook");
+const podcastRoute = require("./route/podcast");
+const cartRoute = require("./route/cart");
+const recipeRoute = require("./route/recipe");
+const paystackPayment = require("./route/payment");
+
+// const targetServer = 'http://localhost:5000';
+
+const app = express();
+dotenv.config();
+// Create a rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 6 * 1000, // 15 minutes
+    max: 25, // limit each ip to 100 request per windowMs
+    message: 'Too many request, try again after 15 minutes'
+    // message: `${alert('Too many request, try again after 15min')}`
+});
+
+// Use limiter
+app.use(limiter);
+
+// proxy middleware
+// app.use('/api', createProxyMiddleware({
+//     target: targetServer,
+//     changeOrigin: true,
+//     pathRewrite: {
+//         "^/api": '',
+//     }
+// }));
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+moongoose
+    .connect(process.env.MONGODB_API)
+    .then((console.log("DBCOnnected Successfully")));
+    // .catch();
+ 
+app.get('/', (req, res) => {
+    res.send('Welcome to backend world');
+});
+app.use("/api/users", userRoute);
+app.use("/api/blogs", blogRoute);
+app.use("/api/books", ebookRoute);
+app.use("/api/podcast", podcastRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/recipe", recipeRoute);
+app.use("/api/checkout", paystackPayment);
+
+
+app.listen(5000, () => {
+    console.log(`Server is running`);
+});
---
+++ route/blog.js
@@ -0,0 +1,81 @@
+const Blog = require("../Model/Blog");
+const router = require("express").Router();
+const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
+
+
+
+// POST
+router.post("/post", verifyTokenAndAdmin, async (req, res) => {
+    const newBlog = new Blog ({
+        headline: req.body.headline,
+        description: req.body.description,
+        author: req.body.author,
+    });
+
+    try {
+        const savedBlog = await newBlog.save();
+        res.status(200).json(savedBlog);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+// GET ALL BLOG
+router.get("/", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const blog = await Blog.find();
+        res.status(200).json(blog);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+// GET ONE BLOG
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        res.status(200).json(blog);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+// UPDATE
+router.put("/update/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const blog = await Blog.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true },
+        );
+        res.status(200).json(blog);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+// DELETE
+router.delete("/delete/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Blog.findByIdAndDelete(
+            req.params.id
+        );
+        res.status(200).json("Data Deleted Successfully");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+
+module.exports = router;
